34b2eaa0d8b257cc951e64cb6b517334b4e0d061
Firitol <92720354+Firitol@users.noreply.github.com>
2026-03-03 06:55:57 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -6,38 +6,38 @@ const Subscription = require('./Subscription');
 const MessageToken = require('./MessageToken');
 const Payment = require('./Payment');
 
-// User ↔ Match associations
+// User ↔ Match
 User.hasMany(Match, { foreignKey: 'user1Id', as: 'matchesAsUser1' });
 User.hasMany(Match, { foreignKey: 'user2Id', as: 'matchesAsUser2' });
 Match.belongsTo(User, { foreignKey: 'user1Id', as: 'user1' });
 Match.belongsTo(User, { foreignKey: 'user2Id', as: 'user2' });
 
-// User ↔ Message associations
+// User ↔ Message
 User.hasMany(Message, { foreignKey: 'senderId', as: 'sentMessages' });
 Message.belongsTo(User, { foreignKey: 'senderId', as: 'sender' });
 
-// Match ↔ Message associations
+// Match ↔ Message
 Match.hasMany(Message, { foreignKey: 'matchId', as: 'messages' });
 Message.belongsTo(Match, { foreignKey: 'matchId', as: 'match' });
 
-// User ↔ Subscription associations
+// User ↔ Subscription
 User.hasOne(Subscription, { foreignKey: 'userId', as: 'subscription' });
 Subscription.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
-// User ↔ MessageToken associations
+// User ↔ MessageToken
 User.hasOne(MessageToken, { foreignKey: 'userId', as: 'messageTokens' });
 MessageToken.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
-// User ↔ Payment associations
+// User ↔ Payment
 User.hasMany(Payment, { foreignKey: 'userId', as: 'payments' });
 Payment.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
 module.exports = {
-  User,
-  Match,
-  Message,
-  Subscription,
-  MessageToken,
-  Payment,
+  User: User,
+  Match: Match,
+  Message: Message,
+  Subscription: Subscription,
+  MessageToken: MessageToken,
+  Payment: Payment,
   sequelize: require('../lib/database').sequelize
 };
